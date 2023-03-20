9938e76e25ec47eee52e65b36c2f80b5646ceed9
Rev1le <r.nikiy@yandex.ru>
2023-03-20 20:21:33 +0500

    Update backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,30 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use big_file_to_parts as BTF;
+use big_file_to_parts::Options;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn encode_file(file_path: String) -> Result<(), String> {
+    let config = BTF::Config::new('e', &file_path);
+    if let Err(e) = BTF::encode::encode_file(&config.path, config.options) {
+        return Err(format!("{:?}", e));
+    }
+    Ok(())
+}
+
+#[tauri::command]
+async fn decode_file(file_path: String) -> Result<(), String> {
+    let config = BTF::Config::new('e', &file_path);
+    if let Err(e) = BTF::decode::decode_file(&config.path) {
+        return Err(format!("{:?}", e));
+    }
+    Ok(())
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![encode_file, decode_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
